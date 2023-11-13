bd7332a4270b21b5279aecd2f952f88b1d04c3f1
giovanamazetto <138473542+giovanamazetto@users.noreply.github.com>
2023-11-13 09:47:23 -0300

    Update 1_visao_empresa.py

--- pages/1_visao_empresa.py
+++ pages/1_visao_empresa.py
@@ -10,6 +10,7 @@ from PIL import Image
 import plotly.express as px
 import folium
 from streamlit_folium import folium_static
+import datetime
 
 
 st.set_page_config (page_title='Visão Empresa', page_icon='📈', layout='wide')
